5218f748e921a9f6e5cd75eb7cc77f3cf41bb326
dg1223 <alavi1223@hotmail.com>
2023-02-12 18:00:19 -0500

    final push

--- js/withdraw.js
+++ js/withdraw.js
@@ -4,21 +4,34 @@ document.getElementById('btn-withdraw').addEventListener('click', function(){
     const withdrawField = document.getElementById('withdraw-field')
     const newWithdrawAmountString = withdrawField.value
     const newWithdrawAmount = parseFloat(newWithdrawAmountString)
+
+    // step 7: clear the withdraw field
+    withdrawField.value = ''
+    
+    if (isNaN(newWithdrawAmount)){
+        alert('Please provide a valid number')
+        return
+    }
     
     // step 3: get the current withdraw total
     const withdrawTotalElement = document.getElementById('withdraw-total')
     const previousWithdrawTotalString = withdrawTotalElement.innerText
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString)
 
-    // step 4: add numbers to set the total withdraw
-    const currentWithdrawTotal = newWithdrawAmount + previousWithdrawTotal
-    // set the withdraw total
-    withdrawTotalElement.innerText = currentWithdrawTotal
-
     // step 5: get balance current total
     const balanceTotalElement = document.getElementById('balance-total')
     const previousBalanceTotalString = balanceTotalElement.innerText
     const previousBalanceTotal = parseFloat(previousBalanceTotalString)
+
+    if (newWithdrawAmount > previousBalanceTotal){
+        alert('Baap er bank e ato taka nai')
+        return
+    }
+
+    // step 4: add numbers to set the total withdraw
+    const currentWithdrawTotal = newWithdrawAmount + previousWithdrawTotal
+    // set the withdraw total
+    withdrawTotalElement.innerText = currentWithdrawTotal
 
     // step 6: calculate current total balance
     const newBalanceTotal = previousBalanceTotal - currentWithdrawTotal
