1ff1006528b31c898568560b318e6435ffdc3d9b
dg1223 <alavi1223@hotmail.com>
2023-02-12 02:28:19 -0500

    navigate to new page

--- js/login.js
+++ js/login.js
@@ -11,9 +11,9 @@ document.getElementById('btn-submit').addEventListener('click', function(){
     // DANGER: DO NOT VERIFY email and password on the client side
     // step 4: verify email and password
     if (email === 'sontan@baap.com' && password === 'secret'){
-        console.log('valid user')
+        window.location.href = 'bank.html'
     }
     else {
-        console.log('invalid user')
+        alert('Wrong username and password combination')
     }
 })
